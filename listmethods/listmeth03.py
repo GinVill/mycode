c154389589f6b8ef034a8ad3d201ab3a43b5763b
Gina Villegas <ginayvillegas@gmail.com>
2021-01-04 22:07:40 +0000

    manipulating lists

---
+++ listmethods/listmeth03.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#Creating list of things I cannot eat
+bad_foods_for_me = ["dairy"]
+bad_foods_for_me.append("mushrooms")
+bad_foods_for_me.append("gluten")
+bad_foods_for_me.append("lentils")
+print(bad_foods_for_me)
+##reversed=bad_foods_for_me.reverse()
+print(bad_foods_for_me.sort())
+
+icecream = ["flavors", "salty"]
+icecream.append(99)
+print(icecream)
+name = input("What is your name?")
+print(f"{icecream[2]} {icecream[0]} and {name} chooses to be {icecream[1]}")
+
