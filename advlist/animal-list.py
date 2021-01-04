0e5591a19ab43f04b0f1fa548c7c652c0a00e420
Gina Villegas <ginayvillegas@gmail.com>
2021-01-04 22:41:13 +0000

    about complex lists and challenges

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+animals = ["Fox"," Fly", " Ant", " Bee", "Cod", " Cat", " Dog", "Yak", " Cow", " Hen", " Koi", "Hog", "Jay", "Kit"]
+print(animals)
+
+challenge= [1, 2, 3, ["a", "b", "c", [1, 4, 7, "yeet"]]]
+print(f"I think we can all agree that  the Lion King is improved by a good {challenge[3][3][3]}")
+
