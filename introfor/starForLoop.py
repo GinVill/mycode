abecf2544ddd97f6cdc3a812668464109f211342
Gina Villegas <ginayvillegas@gmail.com>
2021-01-06 17:23:16 +0000

    Looping techniques

---
+++ introfor/starForLoop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+for number in range(6):
+    print("*" * number)
+for number in range(4, 0, -1):
+    print("*" * number)
+
+for number in range(6):
+    if number < len(range(6)):
